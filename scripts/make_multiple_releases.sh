beabb0464072175b59281cab825c9d23761fc237
George Marques <george@gmarqu.es>
2026-04-27 18:24:05 -0300

    Add script to build with many Godot versions
    To simplify the release process.

---
+++ scripts/make_multiple_releases.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+PLUGIN_VERSION="$1"
+
+if [ -z "$PLUGIN_VERSION" ]; then
+	echo "Plugin version is required"
+	exit 1
+fi
+
+GODOT_VERSIONS="4.4 4.4.1 4.5 4.5.1 4.5.2 4.6 4.6.1 4.6.2"
+PLUGIN_NAME=godot-storekit2
+
+cd godot
+git fetch
+cd ..
+
+for VERSION in $GODOT_VERSIONS; do
+	echo Making $VERSION...
+	cd godot
+	git switch -d $VERSION-stable
+	cd ..
+	./scripts/generate_headers.sh
+	./scripts/make_release.sh
+
+	mv bin/$PLUGIN_NAME.zip bin/$PLUGIN_NAME-$PLUGIN_VERSION-Godot-$VERSION.zip
+done
+
